b72f3c47fdfc7430af8d2b3e9d3746ca7a85ebed
gadmel <abramov.gleb@gmail.com>
2023-03-17 16:26:22 +0100

    Success! - Adjust trigger on pull to main and recheck frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
          <button onClick={handleFetchApi}>
-          Say "Hello" !!
+          Say "Hello" to the API
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
