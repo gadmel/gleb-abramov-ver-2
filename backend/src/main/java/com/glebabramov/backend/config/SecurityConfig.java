b566b49669bb8cf38ca6911b76ad968500307855
gadmel <abramov.gleb@gmail.com>
2023-03-22 23:23:23 +0100

    Integrate a mongodb cluster for future user management, spring securit and its test deps and create a security config

---
+++ backend/src/main/java/com/glebabramov/backend/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.glebabramov.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		return new InMemoryUserDetailsManager(
+				User.builder()
+						.username("Company")
+						.password("CompanyShort-SomeHash")
+						.roles("BASIC")
+						.build()
+		);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
