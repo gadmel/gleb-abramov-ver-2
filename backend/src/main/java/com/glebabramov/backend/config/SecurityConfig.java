8de04c4b22eb7e1c5bdd6945186711ef2656934e
gadmel <abramov.gleb@gmail.com>
2023-03-22 23:41:59 +0100

    Add filter chain to security config

--- backend/src/main/java/com/glebabramov/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/glebabramov/backend/config/SecurityConfig.java
@@ -3,23 +3,28 @@ package com.glebabramov.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		return new InMemoryUserDetailsManager(
-				User.builder()
-						.username("Company")
-						.password("CompanyShort-SomeHash")
-						.roles("BASIC")
-						.build()
-		);
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf().disable()
+				.httpBasic().and()
+				.authorizeHttpRequests()
+				.requestMatchers(HttpMethod.POST, "/api/**").authenticated()
+				.requestMatchers(HttpMethod.PUT, "/api/**").authenticated()
+				.requestMatchers(HttpMethod.POST, "/api/users/login").permitAll()
+				.anyRequest().permitAll()
+				.and()
+				.formLogin()
+				.and().build();
 	}
 
 	@Bean
