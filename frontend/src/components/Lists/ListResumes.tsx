a263e85c3a6c1c71f36b8588d1fd3accef4f84cb
gadmel <abramov.gleb@gmail.com>
2023-04-11 11:30:47 +0200

    Smallfix #2(!)

--- frontend/src/components/Lists/ListResumes.tsx
+++ frontend/src/components/Lists/ListResumes.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEdit} from "@fortawesome/free-solid-svg-icons";
 import {Resume} from "../../services/resumeService";
 import DeleteButtonResume from "../Buttons/InlineButtonDeleteResume";
 import {User} from "../../services/authenticationService";
@@ -18,12 +15,6 @@ type Props = {
 function ListResumes(props: Props) {
 	const standardResumeId = "8c687299-9ab7-4f68-8fd9-3de3c521227e"
 
-	const navigate = useNavigate()
-
-	const handleNavigateToEditPage = (resume: Resume) => {
-		navigate("/secured/cv/", {state: {resume}})
-	}
-
 	const listAssignedUsersNames = (resume: Resume) => {
 		return props.users.filter((user: User) => resume.userIds?.includes(user.id))
 			.map((user: User) => user.username)
@@ -37,10 +28,6 @@ function ListResumes(props: Props) {
 					<div className="resume" key={resume.id}>
 						<p>{resume.name}</p>
 						<p>{listAssignedUsersNames(resume)}</p>
-						<button className="action-button action-button--standard"
-								  onClick={() => handleNavigateToEditPage(resume)}>
-							<FontAwesomeIcon icon={faEdit}/>
-						</button>
 						{resume.id !== standardResumeId &&
                       <DeleteButtonResume id={resume.id} refreshData={props.refreshData}/>}
 					</div>
